78d13a10e077f66ee0012e6f60f9b89a893923c2
skif-38 <medvedpro24@gmail.com>
2026-03-04 16:50:36 +0800

    сделали функцию ресонабл и проверили рабртоспособность

--- tester/testing.py
+++ tester/testing.py
@@ -1,7 +1,9 @@
 import sys
 sys.path.append("../src")
 
-def test_assition():
+from match_demo import add, add_with_bug
+
+def test_addition():
     assert add(1, 3) == 4
     print("test Norm")
 
@@ -11,18 +13,24 @@ def test_addition_with_bug():
     print("Test BUGGED ADDITION PASSED (does it mean code ok?)")
     #assert add_with_bug(6, 7) == 13 # will fail here
 
-def test_addition_duplicated():
-    # is it real good test (relies on absence of + in add())
-    assert add(2, 3) == 2 + 3
-
 def test_addition_overcomplicated():
-    for i in range(0, 2**32):
-        for j in range(0, 2**32):
+    # formally valid test but too slow
+    for i in range(0, 5):
+        for j in range(0, 5):
             assert add(i, j) == sum([i, j])
             assert add(-i, j) == sum([-i, j])
             assert add(i, -j) == sum([i, -j])
             assert add(-i, -j) == sum([-i, -j])
 
+def test_addition_reasonable():
+    assert add(2, 2) == 4
+    assert add(0, 0) == 0
+    assert add(6, 7) == 13
+    assert add(-6, -7) == -13
+    assert add(6, -7) == -1
 
 if __name__ == "__main__":
-    test_addition()  # исправлено, так как такой функции нет в коде
+    test_addition_reasonable()
+    test_addition_overcomplicated()
+    test_addition()
+    test_addition_with_bug()
