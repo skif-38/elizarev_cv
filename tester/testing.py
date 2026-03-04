07322514ef8870671f7dc0f8a6b7b143c85a9af6
skif-38 <medvedpro24@gmail.com>
2026-03-04 16:15:08 +0800

    тест не гарантирует отсутствие ошибок

--- tester/testing.py
+++ tester/testing.py
@@ -4,7 +4,14 @@ sys.path.append("../src")
 from match_demo import add
 
 def test_assition():
-    assert add(2, 2) == 4
+    assert add(1, 3) == 4
     print("test Norm")
+
+def test_addition_with_big():
+    assert test_addition_with_big(2, 4) == 4
+    print("test ok")   
+
+
+
 if __name__ == "__main__":
     test_assition()
